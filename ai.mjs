0dc2d65e8da673b44aa8dee818c89ef9adf58767
StakLabs <staklabsofficial@gmail.com>
2026-09-23 14:37:50 +1000

    Update ai.mjs

--- ai.mjs
+++ ai.mjs
@@ -23,6 +23,8 @@ const allowedOrigins = [
   'http://127.0.0.1:5500',
   'http://localhost:5500',
   'https://staklabs.github.io'
+  'https://sites.google.com',
+  'https://ayaan-creator-web-2.github.io'
 ];
 
 const corsOptions = {
